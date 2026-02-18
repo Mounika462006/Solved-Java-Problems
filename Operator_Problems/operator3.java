73982db1818240e8d2e5a3f9ec92e98593d574ee
Mounika462006 <mounikanagarajan04@gmail.com>
2026-02-18 19:48:42 +0530

    Add files via upload

---
+++ Operator_Problems/operator3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class operator3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int workingHours = sc.nextInt();
+        int attendance = sc.nextInt();
+        if(workingHours>=40 && attendance >=90){
+            System.out.println("Bonus Eligible");
+        }else{
+            System.out.println("Bonus Not Eligible");
+        }
+    }
+}
