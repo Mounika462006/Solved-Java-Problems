852d5ee66d590cacad9817a6c5196df84d16f3ab
Mounika462006 <mounikanagarajan04@gmail.com>
2026-02-18 20:11:13 +0530

    Add files via upload

---
+++ Operator_Problems/operator7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class operator7 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int cartValue = sc.nextInt();
+        int premium = sc.nextInt();
+        if(cartValue>999 || premium==1){
+            System.out.println("Free Shipping");
+        }else{
+            System.out.println("Charges Applied");
+        }
+    }
+}
