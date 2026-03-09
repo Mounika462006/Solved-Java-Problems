d68138a14bc0836e401ec171df7f11ef132a798c
Mounika Nagrajan <mounikanagarajan04@gmail.com>
2026-03-09 22:27:55 +0530

    Add files via upload

---
+++ WhileLoop_Problems/while4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class while4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int fact =1;
+        int i=n;
+        while (i>0) {
+            fact = fact *i;
+            i--;
+        }
+        System.out.println("Factorial of " + n + " = " + fact);
+
+    }
+}
