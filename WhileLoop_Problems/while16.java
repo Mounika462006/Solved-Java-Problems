8f79eef66c82663754ba1010e89e8ce5f6999da3
Mounika Nagrajan <mounikanagarajan04@gmail.com>
2026-03-14 19:33:43 +0530

    Create while16.java

---
+++ WhileLoop_Problems/while16.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int binary = sc.nextInt();
+        int original = binary;
+        int decimal = 0;
+        int power = 0;
+
+        while (binary > 0) {
+            int digit = binary % 10;   
+            decimal += digit * Math.pow(2, power);
+            power++;
+            binary = binary / 10; 
+        }
+
+        System.out.println("Binary: " + original);
+        System.out.println("Decimal: " + decimal);
+    }
+}
