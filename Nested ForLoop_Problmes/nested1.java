d29259c93b386abc2362a3725760e8bfc471b148
Mounika Nagrajan <mounikanagarajan04@gmail.com>
2026-06-06 20:30:52 +0530

    Update nested1.java

--- Nested ForLoop_Problmes/nested1.java
+++ Nested ForLoop_Problmes/nested1.java
@@ -8,7 +8,7 @@ public class nested1{
         System.out.println("Seating Arrangement");
         for(int i=1; i<=row ;i++){
             System.out.print("Row " + i + ": ");
-            for(int j=1;j<=col;j++){
+            for(int j=1;j<=col;j++){     
                
                 System.out.print(" Seat -" + j);
             }
