406d110cf28b03a219aa480033ab35f2517c7d5b
Mounika Nagrajan <mounikanagarajan04@gmail.com>
2026-07-08 21:09:49 +0530

    Refactor ifelse12.java for improved readability

--- Ifelse_Problems/ifelse12.java
+++ Ifelse_Problems/ifelse12.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ifelse12 {
+public class ifelse12 {                                 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
