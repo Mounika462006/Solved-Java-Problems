d0fb1aa348bc37bec31d733a80a83a9a1ed7303f
Mounika Nagrajan <mounikanagarajan04@gmail.com>
2026-07-31 22:38:01 +0530

    Create Number1.java

---
+++ Number_Crunching/Number1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long count =0;
+        
+        while(n>0){
+            long temp = n%10;
+            count++;
+            n=n/10;
+        }
+        System.out.print(count);
+    }
+}
