0c881fd98c58878360527e2b37e0d686066bf076
Mounika Nagrajan <mounikanagarajan04@gmail.com>
2026-08-03 22:01:48 +0530

    Refactor Number10.java for cleaner code
    Removed unnecessary whitespace and simplified the loop condition.

--- Number_Crunching/Number10.java
+++ Number_Crunching/Number10.java
@@ -3,9 +3,7 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         long n = sc.nextLong();
-        
         long last = n%10;
-        
         while(n>=10){
              n=n / 10;
         }
