22d80ccb89d9d7d38cdf5c40e4e225aa36e83a67
Mounika Nagrajan <mounikanagarajan04@gmail.com>
2026-09-24 22:56:09 +0530

    Update nested5.java

--- Nested ForLoop_Problmes/nested5.java
+++ Nested ForLoop_Problmes/nested5.java
@@ -4,7 +4,7 @@ public class Main{
         Scanner sc = new Scanner(System.in);
         long n = sc.nextLong();
         
-        for(int i=1; i<=n;i++){
+        for(int i=1; i<=n;i++){     
             for(int j=1; j<=n;j++){
                 System.out.print("*");
             }
