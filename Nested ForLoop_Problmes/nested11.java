139d46568979ee0962d1ee067060df513b708759
Mounika Nagrajan <mounikanagarajan04@gmail.com>
2026-09-24 22:55:27 +0530

    Update nested11.java

--- Nested ForLoop_Problmes/nested11.java
+++ Nested ForLoop_Problmes/nested11.java
@@ -3,7 +3,7 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i=1; i<=n;i++){
+        for(int i=1; i<=n;i++){           
             for(int j=i; j<=n; j++){
                 System.out.print(j);
             }
