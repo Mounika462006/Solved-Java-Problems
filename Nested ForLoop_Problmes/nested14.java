b0581af0c1f3952ed253191bcabb70e20dd4f24c
Mounika Nagrajan <mounikanagarajan04@gmail.com>
2026-09-26 21:19:05 +0530

    Update nested14.java

--- Nested ForLoop_Problmes/nested14.java
+++ Nested ForLoop_Problmes/nested14.java
@@ -5,7 +5,7 @@ public class Main{
         int n= sc.nextInt();
         for(int i=n; i>0; i--){
             for(int j=1; j<=i;j++){
-                System.out.print(j);
+                System.out.print(j); 
             }
             System.out.print("\n");
         }
